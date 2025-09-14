fd743dc1743f09ca8cb577e3e226b9e91dc4cfb7
kirill <kirisa49@gmail.com>
2025-09-14 15:04:44 +0700

    fixed access_token

--- app/core/auth_key.py
+++ app/core/auth_key.py
@@ -1,6 +1,7 @@
-from jwt import encode, decode, ExpiredSignatureError, InvalidTokenError
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, Cookie
 from fastapi.security import OAuth2PasswordBearer
+
+from jwt import encode, decode, ExpiredSignatureError, InvalidTokenError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import settings
@@ -12,7 +13,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 def generate_access_token(payload: dict) -> str:
     return encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
-def vefiry_access_token(token: str) -> dict:
+def verify_access_token(token: str) -> dict:
     try:
         decoded = decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         return decoded
@@ -23,21 +24,31 @@ def vefiry_access_token(token: str) -> dict:
     except InvalidTokenError:
         raise ValueError("Неверный токен")
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+
+async def get_token_from_cookie(access_token: str = Cookie(None)):
+    if not access_token:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    return access_token
+
+async def get_current_user(token: str = Depends(get_token_from_cookie), db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = vefiry_access_token(token)
+        payload = verify_access_token(token)
         username: str = payload.get("sub")
+
         if username is None:
             raise credentials_exception
+        
     except Exception:
         raise credentials_exception
 
     user = await UserCRUD.get_user_by_username(db, username)
     if user is None:
         raise credentials_exception
+    
     return user
